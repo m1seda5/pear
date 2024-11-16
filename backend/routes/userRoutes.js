20d44eb7cf64b18e07ede2ff2002e949987bd47f
Miseda Eliam <misedaeliam@Misedas-MacBook-Pro.local>
2024-11-16 21:13:39 +0300

    this is the git commit changing the usercontroller user routes and user page where the typerro cannot read properfites bug is being fixed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,33 @@
-// working version
+// // working version (this is og og)
+// import express from "express";
+// import {
+// 	followUnFollowUser,
+// 	getUserProfile,
+// 	loginUser,
+// 	logoutUser,
+// 	signupUser,
+// 	updateUser,
+// 	getSuggestedUsers,
+// 	freezeAccount,
+// } from "../controllers/userController.js";
+// import protectRoute from "../middlewares/protectRoute.js";
+
+// const router = express.Router();
+
+// router.get("/profile/:query", getUserProfile);
+// router.get("/suggested", protectRoute, getSuggestedUsers);
+// router.post("/signup", signupUser);
+// router.post("/login", loginUser);
+// router.post("/logout", logoutUser);
+// router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
+// router.put("/update/:id", protectRoute, updateUser);
+// router.put("/freeze", protectRoute, freezeAccount);
+
+// export default router;
+
+
+
+// bug fixing 
 import express from "express";
 import {
 	followUnFollowUser,
@@ -24,40 +53,3 @@ router.put("/update/:id", protectRoute, updateUser);
 router.put("/freeze", protectRoute, freezeAccount);
 
 export default router;
-
-// verification update
-// import express from "express";
-// import {
-//   followUnFollowUser,
-//   getUserProfile,
-//   loginUser,
-//   logoutUser,
-//   signupUser,
-//   updateUser,
-//   getSuggestedUsers,
-//   freezeAccount,
-//   awardVerification
-// } from "../controllers/userController.js";
-// import protectRoute from "../middlewares/protectRoute.js";
-// import roleAssignment from "../middlewares/roleAssignment.js"; // Import the roleAssignment middleware
-
-// const router = express.Router();
-
-// // User routes
-
-// // Signup route (roleAssignment will run first)
-// router.post("/signup", roleAssignment, signupUser);
-
-// // Other routes
-// router.get("/profile/:query", getUserProfile);
-// router.get("/suggested", protectRoute, getSuggestedUsers);
-// router.post("/login", loginUser);
-// router.post("/logout", logoutUser);
-// router.post("/follow/:id", protectRoute, followUnFollowUser); // Toggle state(follow/unfollow)
-// router.put("/update/:id", protectRoute, updateUser);
-// router.put("/freeze", protectRoute, freezeAccount);
-
-// // New route for awarding verification
-// router.post("/awardVerification", protectRoute, awardVerification);
-
-// export default router;
