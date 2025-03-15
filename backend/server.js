9993afddc203a015913f11caecea1623b52e1e28
ktiuyi <nkituyi@stidents.brookhouse.ac.ke>
2025-03-15 11:53:42 +0300

    this is the updating the server still the same update the post reviewer systme and then admin dashboard  removed te dupicated spxress thing

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,3 @@
-// routes/reviewerGroupRoutes.js
-import express from "express";
-import {
-  createReviewerGroup,
-  getReviewerGroups,
-  updateReviewerGroup,
-  deleteReviewerGroup,
-  addMemberToGroup,
-  removeMemberFromGroup
-} from "../controllers/reviewerGroupController.js";
-import protectRoute from "../middlewares/protectRoute.js";
-import adminMiddleware from "../middlewares/adminMiddleware.js";
-
-const router = express.Router();
-
-router.post("/", protectRoute, adminMiddleware, createReviewerGroup);
-router.get("/", protectRoute, adminMiddleware, getReviewerGroups);
-router.put("/:id", protectRoute, adminMiddleware, updateReviewerGroup);
-router.delete("/:id", protectRoute, adminMiddleware, deleteReviewerGroup);
-router.post("/:groupId/members", protectRoute, adminMiddleware, addMemberToGroup);
-router.delete("/:groupId/members/:userId", protectRoute, adminMiddleware, removeMemberFromGroup);
-
-export default router;
 
 // Update server.js to include reviewerGroupRoutes
 import path from "path";
