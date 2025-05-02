1ff968d2e416cf0086443d1f0275c304c6d6eda1
ktiuyi <nkituyi@stidents.brookhouse.ac.ke>
2025-05-02 19:30:36 +0300

    removed notification provider

--- frontend/src/hooks/useShowToast.js
+++ frontend/src/hooks/useShowToast.js
@@ -2,14 +2,15 @@ import { useContext, useCallback } from "react";
 import { NotificationContext } from "../providers/NotificationProvider";
 
 const useShowToast = () => {
-	const { showNotification } = useContext(NotificationContext);
-	// Usage: showToast(title, description, status)
-	return useCallback(
-		(title, description, status) => {
-			showNotification({ title, description, status });
-		},
-		[showNotification]
-	);
+	return (title, description, status) => {
+		if (status === 'error') {
+			toasts.service.error(title, 'alert-circle', description, 'topRight', 5000);
+		} else if (status === 'success') {
+			toasts.service.success(title, 'check-circle', description, 'topRight', 5000);
+		} else {
+			toasts.service.info(title, 'info', description, 'topRight', 5000);
+		}
+	};
 };
 
 export default useShowToast;
