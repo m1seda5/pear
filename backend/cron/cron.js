0f409fd4df908d43624c5145b6aad2f1aab25f1d
ktiuyi <nkituyi@stidents.brookhouse.ac.ke>
2025-05-13 21:25:58 +0300

    nodecron

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,55 +1,41 @@
-import cron from "cron";
+import nodeCron from "node-cron";
 import https from "https";
 import { sendSmartNotifications, sendNoPostsTodayNotification } from "../controllers/notificationController.js";
 
 const URL = "https://pear-tsk2.onrender.com";
 
-// Existing keep-alive job
-const keepAliveJob = new cron.CronJob("*/14 * * * *", function() {
-  https
-    .get(URL, (res) => {
-      if (res.statusCode === 200) {
-        console.log("GET request sent successfully");
-      } else {
-        console.log("GET request failed", res.statusCode);
-      }
-    })
-    .on("error", (e) => {
-      console.error("Error while sending request", e);
-    });
-});
-
-// Schedule smart notifications for inactive users at 7:30 AM and 7:30 PM
-cron.schedule('30 7 * * *', () => {
-  sendSmartNotifications();
-});
-cron.schedule('30 19 * * *', () => {
-  sendSmartNotifications();
-});
-// Schedule no-posts-today notification for 8:30 PM
-cron.schedule('30 20 * * *', () => {
-  sendNoPostsTodayNotification();
-});
-
+// Start all jobs immediately
 export default {
   start: () => {
-    keepAliveJob.start();
+    // Keep-alive job
+    nodeCron.schedule("*/14 * * * *", function() {
+      https
+        .get(URL, (res) => {
+          if (res.statusCode === 200) {
+            console.log("GET request sent successfully");
+          } else {
+            console.log("GET request failed", res.statusCode);
+          }
+        })
+        .on("error", (e) => {
+          console.error("Error while sending request", e);
+        });
+    });
+
+    // Smart notifications
+    nodeCron.schedule('30 7 * * *', () => {
+      sendSmartNotifications();
+    });
+    
+    nodeCron.schedule('30 19 * * *', () => {
+      sendSmartNotifications();
+    });
+    
+    // No-posts-today notification
+    nodeCron.schedule('30 20 * * *', () => {
+      sendNoPostsTodayNotification();
+    });
+    
     console.log("All cron jobs started");
   }
 };
-
-// CRON JOB EXPLANATION:
-// Cron jobs are scheduled tasks that run periodically at fixed intervals or specific times
-// send 1 GET request for every 14 minutes
-
-// Schedule:
-// You define a schedule using a cron expression, which consists of five fields representing:
-
-//! MINUTE, HOUR, DAY OF THE MONTH, MONTH, DAY OF THE WEEK
-
-//? EXAMPLES && EXPLANATION:
-//* 14 * * * * - Every 14 minutes
-//* 0 0 * * 0 - At midnight on every Sunday
-//* 30 3 15 * * - At 3:30 AM, on the 15th of every month
-//* 0 0 1 1 * - At midnight, on January 1st
-//* 0 * * * * - Every hour
