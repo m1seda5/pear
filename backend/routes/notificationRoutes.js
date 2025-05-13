e9088abe1f187adf9398406783bbb3fe57e09a27
ktiuyi <nkituyi@stidents.brookhouse.ac.ke>
2025-05-13 21:19:43 +0300

    no idle notifications

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -1,8 +1,6 @@
 // routes/notificationRoutes.js
 import express from "express";
-import { sendIdleNotifications } from "../controllers/notificationController.js";
 import adminMiddleware from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
-router.post("/test-notifications", adminMiddleware, sendIdleNotifications);
 export default router;
