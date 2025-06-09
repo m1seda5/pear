64f715037a3fcbb57075c8fc92a59252255ebfc1
ktiuyi <nkituyi@stidents.brookhouse.ac.ke>
2025-06-09 16:34:39 -0400

    no more notifications

--- backend/cron.js
+++ backend/cron.js
@@ -19,39 +19,39 @@ const keepAliveJob = new cron.CronJob("*/14 * * * *", function() {
     });
 });
 
-// Morning notifications (7:30 - 8:45)
-const morningJob = new cron.CronJob(
-  '30 7 * * 1-5', // At 7:30 AM on weekdays
-  sendIdleNotifications,
-  null,
-  true,
-  'Africa/Nairobi'
-);
+// // Morning notifications (7:30 - 8:45)
+// const morningJob = new cron.CronJob(
+//   '30 7 * * 1-5', // At 7:30 AM on weekdays
+//   sendIdleNotifications,
+//   null,
+//   true,
+//   'Africa/Nairobi'
+// );
 
-// Afternoon notifications (1:40 - 3:35)
-const afternoonJob = new cron.CronJob(
-  '40 13 * * 1-5', // At 1:40 PM on weekdays
-  sendIdleNotifications,
-  null,
-  true,
-  'Africa/Nairobi'
-);
+// // Afternoon notifications (1:40 - 3:35)
+// const afternoonJob = new cron.CronJob(
+//   '40 13 * * 1-5', // At 1:40 PM on weekdays
+//   sendIdleNotifications,
+//   null,
+//   true,
+//   'Africa/Nairobi'
+// );
 
-// Saturday special notification
-const saturdayJob = new cron.CronJob(
-  '30 12 * * 6', // Saturday at 12:30 PM
-  () => sendIdleNotifications(),
-  null,
-  true,
-  'Africa/Nairobi'
-);
+// // Saturday special notification
+// const saturdayJob = new cron.CronJob(
+//   '30 12 * * 6', // Saturday at 12:30 PM
+//   () => sendIdleNotifications(),
+//   null,
+//   true,
+//   'Africa/Nairobi'
+// );
 
 export default {
   start: () => {
     keepAliveJob.start();
-    morningJob.start();
-    afternoonJob.start();
-    saturdayJob.start();
+    // morningJob.start();
+    // afternoonJob.start();
+    // saturdayJob.start();
     console.log("All cron jobs started");
   }
 }; 
